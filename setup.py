ebe7c527df256271755ffb3893a21dbbb33b0b51
prasadmd83 <prasadmd83@users.noreply.github.com>
2018-12-16 16:10:39 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='gill',
       version='0.0.1',
       description='A sample PySpark application',
-      url='http://github.com/mrpowers/gill',
-      author='MrPowers',
-      author_email='matthewkevinpowers@gmail.com',
+      url='',
+      author='PrasadDeshpande',
+      author_email='prasadm.deshpande@gmail.com',
       packages=['gill'],
       zip_safe=False)
